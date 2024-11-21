17833ce6bfe8bceb007f39b7118cafe0495782f0
amagyei <kwame.na@hotmail.com>
2024-11-21 15:29:02 +0000

    task 12

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -49,11 +49,9 @@ class Auth:
             return None
 
         try:
-            user = self._db.find_user_by(session_id=session_id)
+            return self._db.find_user_by(session_id=session_id)
         except NoResultFound:
             return None
-
-        return user
         
     
     def destroy_session(self, user_id):
