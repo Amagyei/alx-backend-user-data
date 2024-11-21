67ed8cdb83727375efa635c8f3c7000e2dc8ead9
amagyei <kwame.na@hotmail.com>
2024-11-21 15:13:58 +0000

    task 12

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -48,7 +48,7 @@ class Auth:
         if not session_id:
             return None
         try:
-            user = self._db.find_user_by(session_id=session_id)
+            user = self._db.find_user_by(id=session_id)
         except NoResultFound:
             return None
 
